b24b7c59d9bc011cf0f7fbd7bb68c859aa118175
e <17310051361@163.com>
2018-04-16 16:40:13 +0800

    fix code

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,7 @@ class UsersController extends Controller
    
 	public function __construct()
 	{
-		$this->middleware('auth', ['except' => ['show']])；
+		$this->middleware('auth', ['except' => ['show']]);
 	}
 
 
