4bfbcd8320049f387fd6acc4b243f6a49bea0923
e <17310051361@163.com>
2018-04-16 16:59:27 +0800

    用户、专题、文章seed生成

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('posts', 'PostsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
