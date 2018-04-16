c105d594489b2d59984f63ac3baa6ef47630c748
e <17310051361@163.com>
2018-04-16 16:36:10 +0800

    用户编辑资料

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,8 +1,9 @@
 @extends("layouts.app")
 @section("content")
  <div class="col-sm-8 blog-main">
-        <form class="form-horizontal" action="/user/5/setting" method="POST" enctype="multipart/form-data">
-            <input type="hidden" name="_token" value="RuzO8giZVe3C2PalHpxGydYXKvwqNxMwcxscznAb">
+        <form class="form-horizontal" action="{{ route('users.update', $user->id) }}" method="POST" enctype="multipart/form-data">
+            <input type="hidden" name="_method" value="PUT">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <div class="form-group">
                 <label class="col-sm-2 control-label">用户名</label>
                 <div class="col-sm-10">
@@ -12,8 +13,8 @@
             <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-2">
-                    <input class=" file-loading preview_input" type="file" value="用户名" style="width:72px" name="avatar">
-                    <img  class="preview_img" src="image/user.jpeg" alt="" class="img-rounded" style="border-radius:500px;">
+                    <input  type="file" value="用户名"  name="avatar">
+                    
                 </div>
             </div>
             <button type="submit" class="btn btn-default">修改</button>
